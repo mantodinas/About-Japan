05d8268bef68bd967b9c830d153e0a8e37b9a31b
mantodinas <82802179+mantodinas@users.noreply.github.com>
2021-11-28 21:29:13 +0200

    small update

--- JS/home.js
+++ JS/home.js
@@ -20,6 +20,7 @@ window.addEventListener("scroll", () => {
     }
 })
 // Scroll up button END
+
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("myTopnav");
@@ -29,3 +30,4 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+/* END */
